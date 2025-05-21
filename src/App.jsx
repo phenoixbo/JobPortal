7ecb8dc6fd85e773e87687c818d7e012b3dbb958
Pradeep <pradeepganesan034@gmail.com>
2025-05-21 09:58:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import JobDetails from "./pages/JobDetails";
@@ -9,16 +9,14 @@ import "./styles/styles.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/job/:id" element={<JobDetails />} />
-        <Route path="/post-job" element={<PostJob />} />
-        <Route path="/jobapplyform-apply" element={<JobApplyForm />} />
-        <Route path="/joblist" element={<JobList />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/job/:id" element={<JobDetails />} />
+      <Route path="/post-job" element={<PostJob />} />
+      <Route path="/jobapplyform-apply" element={<JobApplyForm />} />
+      <Route path="/joblist" element={<JobList />} />
+    </Routes>
   );
 }
 
